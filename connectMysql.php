ede6a13e87a7a0a81092e908354fd6de3f18afe6
Trương Lựu <LuKa@Macintosh.local>
2020-01-18 16:29:12 +0700

    can not login to mysql by php

--- connectMysql.php
+++ connectMysql.php
@@ -1,11 +1,11 @@
 <?php
-$host="http://cafesland.com:2082";
-$user="cafe9899";
-$pass="8@zuT*wVXtyz17,";
-$con=mysql_connect($host,$user,$pass);
-if($con==1){
+$host='https://s2d253.cloudnetwork.vn:2083';
+$user='cafe9899';
+$pass='8@zuT*wVXtyz17,';
+$con=mysqli_connect($host,$user,$pass);
+if($con){
   echo "login ok";
 }else{
-  echo "login not ok";
+  echo "login not 2 ok";
 }
- ?>
+?>
