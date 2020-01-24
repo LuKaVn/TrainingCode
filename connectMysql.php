c35e3af5857ba7c729761a0841197e6b640efd9b
Trương Lựu <LuKa@Macintosh.local>
2020-01-24 15:04:03 +0700

    24/01/2020

--- connectMysql.php
+++ connectMysql.php
@@ -1,35 +1,22 @@
 
-<?php/*
-$servername = "localhost";
-$username = "cafe9899";
+<?php
+$server = "localhost";
+$user = "cafe9899";
 $password = "8@zuT*wVXtyz17,";
-$dbname = "cafe9899_UVmeter";
-$conn=mysqli_connect($servername,$username,$password,$dbname);
-if($conn){
-  echo "login ok";
-  //CREATE DATABASE lukadatabse;
+$nameDB = "cafe9899_UVmeter";
+
+$conn=mysqli_connect($server,$user,$password,$nameDB);
+if($conn==1){
+  echo("connect Ok");
 }else{
-  echo "login not 3.1 ok";
+  echo("connect not ok");
 }
 
-if ($conn->query('CREATE TABLE data(
-ID int(6), name VARCHAR(30)
-)') == TRUE) {
-      echo "tao bang OK";
-      //echo "Thêm dữ liệu thành công <br>";
-    } else {
-      echo "Error: " . $sql . "<br>" . $conn->error;
-    }
-$conn->close();
-*/
-$server = 'localhost';
-$user="cafe9899";
-$password="8@zuT*wVXtyz17";
-$nameDB="cafe9899_UVmeter";
-$conn=mysqli_connect($server,$user,$password,$nameDB);
-if(conn==1){
-  echo("login OK");
+$drop=$conn->query("DROP TABLE LUKA3");
+if($drop==true){
+  echo("delete OK");
 }else{
-  echo("login not ok");
+  echo("delete not ok");
 }
+$conn->close();
 ?>
